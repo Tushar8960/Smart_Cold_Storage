773d5edb510c890497130bb29a1acc60bd74c09f
Tushar <tusharkaushal2006@gmail.com>
2026-08-29 20:49:48 +0000

    second commit for smart cold storage

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Smart_Cold_Storage/",
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      // Forwards /api/* calls to your backend during local dev
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
       },
     },
   },
-})
+});
